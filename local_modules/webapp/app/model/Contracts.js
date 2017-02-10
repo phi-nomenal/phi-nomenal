32a4ad27615e96a68035e17e3e6ce4cff230ec05
Spanbroek, Mark <mark@spanbroek.net>
2017-02-10 22:24:44 +0100

    Set Web3 provider on contracts.

--- local_modules/webapp/app/model/Contracts.js
+++ local_modules/webapp/app/model/Contracts.js
@@ -1,3 +1,4 @@
+import Web3 from 'web3'
 import contract from 'truffle-contract'
 
 import RFQRegistryJSON from 'contracts/build/contracts/RFQRegistry.json'
@@ -5,5 +6,9 @@ import RFQJSON from 'contracts/build/contracts/RFQ.json'
 
 const RFQRegistry = contract(RFQRegistryJSON)
 const RFQ = contract(RFQJSON)
+
+const provider = new Web3.providers.HttpProvider('http://localhost:8545')
+RFQRegistry.setProvider(provider)
+RFQ.setProvider(provider)
 
 export { RFQ, RFQRegistry }
