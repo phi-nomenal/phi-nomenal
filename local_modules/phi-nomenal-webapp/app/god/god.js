6b340a3bcea730db3e43210530be7c802fd85d00
Spanbroek, Mark <mark@spanbroek.net>
2017-02-11 21:50:31 +0100

    Added god mode overview.

--- local_modules/phi-nomenal-webapp/app/god/god.js
+++ local_modules/phi-nomenal-webapp/app/god/god.js
@@ -1,36 +1,13 @@
 import React from 'react'
 import { render } from 'react-dom'
-import RfqRegistryModel from '../model/RfqRegistryModel'
+import './god.css'
 
 class God extends React.Component {
 
-  constructor () {
-    super()
-    this.state = { rfqs: [] }
-  }
-
-  componentDidMount () {
-    this.loadOpenRFQs()
-  }
-
-  async loadOpenRFQs () {
-    let registry = await RfqRegistryModel.create()
-    let openRFQs = await registry.openRFQs()
-    this.setState({ rfqs: openRFQs })
-  }
-
   render () {
-    let rfqs = this.state.rfqs
-    return <div>{ rfqs.map(this.renderRfq) }</div>
-  }
-
-  renderRfq (rfq) {
-    return <h1 key={rfq.id}>{
-      'product: ' + rfq.product +
-      ', amount: ' + rfq.amount +
-      ', delivery region: ' +
-      rfq.deliveryRegion
-    }</h1>
+    return <div id='god'>
+      <div id='god-overview' />
+    </div>
   }
 }
 
