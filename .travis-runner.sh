755b3ec2b144542dbfe1b1f7a04f024581f03bf4
Spanbroek, Mark <mark@spanbroek.net>
2017-02-11 11:29:38 +0100

    Fix travis build (hopefully).

--- .travis-runner.sh
+++ .travis-runner.sh
@@ -6,6 +6,8 @@ startdir="$(cd `dirname $0`; pwd)"
 ( cd ${startdir}/local_modules/phi-nomenal-contracts && yarn run testrpc & )
 ( cd ${startdir}/local_modules/phi-nomenal-webapp && yarn install )
 
+yarn install
+
 # Make sure testrpc is fully up and running
 sleep 5
 
