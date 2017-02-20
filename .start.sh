380b60658d6efd7bd8c78be048a28df051ba2a1f
Spanbroek, Mark <mark@spanbroek.net>
2017-02-20 09:39:56 +0100

    Use deployed contracts in webapp.

--- .start.sh
+++ .start.sh
@@ -3,4 +3,6 @@
 ./.testrpc-service.sh &
 sleep 5
 ( cd local_modules/phi-nomenal-contracts && ./node_modules/.bin/truffle migrate )
+./node_modules/.bin/lm install -f
+( cd local_modules/phi-nomenal-webapp && ./node_modules/.bin/webpack )
 ( cd local_modules/phi-nomenal-httpserver && yarn start )
